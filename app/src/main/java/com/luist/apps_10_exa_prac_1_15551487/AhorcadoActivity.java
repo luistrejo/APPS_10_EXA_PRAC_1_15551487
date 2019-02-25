9007e33c9e4d2cd8d813b6a47f247dfe1c352fd0
latm33 <luistrejo033@gmail.com>
2019-02-25 15:16:15 -0700

    Comentarios
    Comentarios añadidos al codigo del Ahorcado

--- app/src/main/java/com/luist/apps_10_exa_prac_1_15551487/AhorcadoActivity.java
+++ app/src/main/java/com/luist/apps_10_exa_prac_1_15551487/AhorcadoActivity.java
@@ -52,6 +52,9 @@ public class AhorcadoActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnOK:
+                //Comparamos si el usuario ingreso una palabra completa con la variable
+                //de la palabra seleccionada, si son iguales mostramos Dialog de ganador
+                //de lo contrario el de perdedor.
                 if (eTxtLetra.getText().toString().equals(sPalabra)) {
                     eTxtPalabra.setText("");
                     eTxtPalabra.setText(sPalabra);
@@ -64,15 +67,20 @@ public class AhorcadoActivity extends AppCompatActivity implements View.OnClickL
                     return;
                 }
 
+                //Verificamos que la palabra seleccionada contenga la letra ingresada
                 if (sPalabra.contains(eTxtLetra.getText().toString())) {
                     Log.d("vocales", isVocal(eTxtLetra.getText().toString().charAt(0)) + "");
+                    //Si esta seleccionada el Check de XTream y la letra es vocal descontamos un intento
                     if (bXTream && isVocal(eTxtLetra.getText().toString().charAt(0))) {
                         iIntentos--;
                         txtVwIntentos.setText(iIntentos + "");
                         if (iIntentos == 0) {
-                            showDialogResultado("HAS GANADO-PERDIDO?", R.drawable.ganador);
+                            showDialogResultado("HAS PERDIDO!!", R.drawable.perdedor);
                         }
                     }
+
+                    //Cambiamos los guiones por la letra ingresada en las posiciones
+                    //que nos regresa el metodo posicionesLetra()
                     char[] cGuiones = sGuiones.toCharArray();
                     for (int i : posicionesLetra(eTxtLetra.getText().toString().charAt(0), sPalabra)) {
                         cGuiones[i] = eTxtLetra.getText().toString().charAt(0);
@@ -80,6 +88,7 @@ public class AhorcadoActivity extends AppCompatActivity implements View.OnClickL
                     sGuiones = String.valueOf(cGuiones);
                     eTxtPalabra.setText(sGuiones);
                     eTxtLetra.setText("");
+                    //Comparamos la palabra seleccionada con el avance
                     if (sPalabra.equals(sGuiones)) {
                         showDialogResultado("HAS GANADO!!", R.drawable.ganador);
                     }
@@ -101,22 +110,30 @@ public class AhorcadoActivity extends AppCompatActivity implements View.OnClickL
         }
     }
 
+    //Metodo que limpia los EditText, reinicia la variable de intentos
+    //y selecciona una nueva palabra
     private void nuevoJuego() {
+        //Limpiamos todos los campos y variables usadas
         eTxtPalabra.setText("");
         eTxtLetra.setText("");
         iIntentos = 0;
         sPalabra = "";
         sGuiones = "";
         bXTream = checkXTream.isChecked();
+
+        //Generamos un numero aleatorio del 0 al 9 para seleccionar una palabra del arreglo
         int max = 9;
         int min = 0;
         int range = max - min + 1;
         int rand = (int) (Math.random() * range) + min;
         sPalabra = arrayPalabras[rand];
         Log.d("PALABRA", sPalabra);
+
+        //Generamos el string de guiones con la longitud de la palabra
         for (int x = 0; x < sPalabra.length(); x++) {
             sGuiones = sGuiones.concat("_");
         }
+        //Revisamos el nivel de juego y lo asignamos a la variable correspondiente
         if (rBtnDificil.isChecked()) {
             iIntentos = 2;
         } else if (rBtnMedio.isChecked()) {
@@ -128,8 +145,10 @@ public class AhorcadoActivity extends AppCompatActivity implements View.OnClickL
         eTxtPalabra.setText(sGuiones);
         txtVwLetras.setText(sPalabra.length() + "");
         txtVwIntentos.setText(iIntentos + "");
+        Toast.makeText(this, sPalabra, Toast.LENGTH_LONG).show();
     }
 
+    //Metodo que regresa las posiciones de una letra en una palabra
     public List<Integer> posicionesLetra(char LetraABuscar, String palabra) {
         List<Integer> posiciones = new ArrayList<>();
         for (int i = 0; i < palabra.length(); i++) {
@@ -160,6 +179,8 @@ public class AhorcadoActivity extends AppCompatActivity implements View.OnClickL
         dlgMiDialogo.show();
     }
 
+    //Metodo que detecta si una letra es vocal
+    //Si no es vocal .indexOf regresa -1
     public static boolean isVocal(char c) {
         return "aeiou".indexOf(c) != -1;
     }
